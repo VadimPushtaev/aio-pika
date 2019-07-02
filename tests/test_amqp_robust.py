7360245f5385e9665f5426875eabb165dba4ce79
Vadim Pushtaev <pushtaev.vm@gmail.com>
2019-07-02 21:00:09 +0300

    test refactoring [garbage]

--- tests/test_amqp_robust.py
+++ tests/test_amqp_robust.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from socket import socket
+from typing import Optional
 
 import aiormq
 from contextlib import suppress
@@ -71,18 +72,22 @@ class Proxy:
     async def disconnect(self):
         tasks = list()
 
-        async def close(writer):
-            writer.close()
-            await writer.wait_closed()
+        async def close(w):
+            w.close()
+            await w.wait_closed()
 
         while self.connections:
-            writer = self.connections.pop()     # type: asyncio.StreamWriter
+            writer: asyncio.StreamWriter = self.connections.pop()
             tasks.append(self.loop.create_task(close(writer)))
 
         await asyncio.wait(tasks)
 
 
 class TestCase(AMQPTestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.proxy: Optional[Proxy] = None
+
     @staticmethod
     def get_unused_port() -> int:
         sock = socket()
@@ -107,7 +112,7 @@ class TestCase(AMQPTestCase):
             self.proxy.src_port
         ).update_query(reconnect_interval=1)
 
-        client = await connect_robust(str(url), loop=self.loop)
+        client = await connect_robust(str(url), loop=self.loop, reconnect_interval=0.1)
 
         if cleanup:
             self.addCleanup(client.close)
@@ -174,61 +179,10 @@ class TestCase(AMQPTestCase):
         self.assertEqual(reconnect_count, 1)
 
     async def test_robust_reconnect__no_downtime(self):
-        await self._test_robust_reconnect(proxy_downtime=0)
+        await RobustConnectTester(self, proxy_downtime=0).run()
 
     async def test_robust_reconnect__with_downtime(self):
-        await self._test_robust_reconnect(proxy_downtime=1)
-
-    async def _test_robust_reconnect(self, *, proxy_downtime):
-        channel1 = await self.create_channel()
-        channel2 = await self.create_channel()
-
-        shared = []
-        queue = await channel1.declare_queue()
-
-        async def reader():
-            nonlocal shared
-            async with queue.iterator() as q:
-                async for message in q:
-                    shared.append(message)
-                    await message.ack()
-
-        async def restart_proxy(proxy):
-            await proxy.disconnect()
-            if proxy_downtime:
-                await proxy.stop()
-                await asyncio.sleep(proxy_downtime, loop=self.loop)
-                await proxy.start()
-
-        reader_task = self.loop.create_task(reader())
-        self.addCleanup(reader_task.cancel)
-
-        for _ in range(5):
-            await channel2.default_exchange.publish(
-                Message(b''), queue.name,
-            )
-
-        logging.info("Disconnect all clients")
-        await restart_proxy(self.proxy)
-
-        logging.info("Waiting for reconnect")
-        await asyncio.sleep(5)
-
-        logging.info("Waiting connections")
-        await asyncio.wait([
-            channel1._connection.ready(),
-            channel2._connection.ready()
-        ])
-
-        for _ in range(5):
-            await channel2.default_exchange.publish(
-                Message(b''), queue.name,
-            )
-
-        while len(shared) < 10:
-            await asyncio.sleep(0.1)
-
-        assert len(shared) == 10
+        await RobustConnectTester(self, proxy_downtime=2).run()
 
     async def test_channel_locked_resource2(self):
         ch1 = await self.create_channel()
@@ -242,3 +196,82 @@ class TestCase(AMQPTestCase):
         with self.assertRaises(ChannelLockedResource):
             q2 = await ch2.declare_queue(qname, exclusive=True, robust=False)
             await q2.consume(print, exclusive=True)
+
+
+class RobustConnectTester:
+    def __init__(self, test_case: TestCase, *, proxy_downtime: int):
+        self._test_case = test_case
+        self._proxy_downtime = proxy_downtime
+
+    async def _reader(self, queue, consumed):
+        async with queue.iterator() as q:
+            async for message in q:
+                consumed.append(message)
+                await message.ack()
+
+    async def _writer(self, channel, queue):
+        for _ in range(1000):
+            await channel.default_exchange.publish(
+                Message(b''), queue.name,
+            )
+
+        return
+
+        while True:
+            try:
+                await channel.default_exchange.publish(
+                    Message(b''), queue.name,
+                )
+            except asyncio.CancelledError:
+                raise
+            except Exception:
+                pass
+            await asyncio.sleep(0.1)
+
+    async def _restart_proxy(self):
+        proxy: Proxy = self._test_case.proxy
+
+        await proxy.disconnect()
+        if self._proxy_downtime:
+            await proxy.stop()
+            await asyncio.sleep(self._proxy_downtime)
+            await proxy.start()
+
+    async def run(self):
+        channel1 = await self._test_case.create_channel()
+        channel2 = await self._test_case.create_channel()
+
+        consumed = []
+        queue = await channel1.declare_queue()
+
+        reader_task = asyncio.get_event_loop().create_task(
+            self._reader(queue, consumed)
+        )
+        self._test_case.addCleanup(reader_task.cancel)
+
+        writer_task = asyncio.get_event_loop().create_task(
+            self._writer(channel2, queue)
+        )
+        self._test_case.addCleanup(writer_task.cancel)
+        await writer_task
+
+        logging.info("Wait for some messages to be processed")
+        while len(consumed) < 10:
+            await asyncio.sleep(0.01)
+
+        logging.info("Disconnect all clients")
+        await self._restart_proxy()
+
+        logging.info("Waiting for reconnect")
+        await asyncio.sleep(5)
+
+        logging.info("Waiting connections")
+        await asyncio.wait([
+            channel1._connection.ready(),
+            channel2._connection.ready()
+        ])
+
+        while len(consumed) < 30:
+            await asyncio.sleep(0.1)
+
+        assert len(consumed) >= 30
